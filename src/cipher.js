0c99d1f9e930b8218097723ccebd1e850ef60d1f
T-Barrios <cherub.nevermind@gmail.com>
2023-02-22 20:07:47 -0300

    cipher con formula para cifrar y descifrar. todo funciona menos npm test

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
-const cipher = {
-  // ...
-};
+const cipher ={
+  encode: function(offset,string){
+    let encoded = ""
+    encoded = (((string-65)+offset)%26)+65
+    return encoded
+  },
+  decode: function(offset,string){
+    let decoded = ""
+    decoded = ((((string-65)+52)-offset)%26)+65
+    return decoded
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,85 +1,65 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-//************************************************************************************************************************ */
-//CIFRAR
-//************************************************************************************************************************ */
+//VARIABLE DE LOS INPUT TEXTO Y NUMERO
+let texto = ""
+let numero = 0
+let xtexto = ""
+let xnumero = 0
 
-//variables de cifrado (texto y numero)
-let inputCipher = "";
-let numberCipher = 0;
-
-//variables de descifrado (texto y número)
-let inputDecipher = "";
-let numberDecipher = 0;
-
-//obtener input de cifrado (texto y numero)
-document.getElementById("inputNormal").addEventListener("input", function(e){
-  inputCipher = e.target.value;
-});
-document.getElementById("numeroCifrar").addEventListener("input", function(e){
-  numberCipher = e.target.value;
-});
-
-//obtener input de descifrado (texto y numero)
-document.getElementById("inputCifrado").addEventListener("input", function(e){
-  inputDecipher = e.target.value;
-});
-document.getElementById("numeroDescifrar").addEventListener("input", function(e){
-  numberDecipher = e.target.value;
-});
-
-//boton de cifrado (boton go)
+//BOTONES CIFRAR Y DESCIFRAR
 const buttonGoCipher = document.getElementById("goCifrar");
 buttonGoCipher.addEventListener("click",cifrar)
+buttonGoCipher.addEventListener("click",mostrarCipherScreen)
 
-//boton de descifrado (boton go)
 const buttonGoDecipher = document.getElementById("goDescifrar");
 buttonGoDecipher.addEventListener("click",descifrar)
+buttonGoDecipher.addEventListener("click",mostrarDecipherScreen)
 
-//función para guardar cada letra del input por separado en un array
-let letterArray = [];
-let letter = ""
-let a = 0
-let z = 1
-
-function getLetter(){
-  for (let i = 0; i < inputCipher.length; i++){
-    letter = inputCipher;
-    let letterCut = letter.slice(a,z);
-    letterArray.push(letterCut);
-    a += 1;
-    z += 1;
-  }
+//FUNCION CIFRAR MENSAJE
+function cifrar(){
+  texto = document.getElementById("inputNormal").value
+  numero = document.getElementById("numeroCifrar").value
+  const letterArray = []
+  const numberArray = []
+  const asciiArray = []
+  let numberToLetter = 0
+  let mensajeCifrado = ""
+  for (let i = 0; i < texto.length; i++){
+    letterArray.push(texto.slice(i,1+i))
+    numberArray.push(letterArray[i].charCodeAt(0))
+    asciiArray.push(cipher.encode((+numero),numberArray[i]))
+    numberToLetter = String.fromCharCode(asciiArray[i])
+    mensajeCifrado += numberToLetter
+  }  
+  return document.getElementById("textoCifrado").innerHTML = mensajeCifrado
 }
 
-//función para convertir cada letra del array en un numero ascii
-let letra = ""
-let asciNumber = 0
-let numberArray = []
-
-function letterToNumber(){
-  for (let i =0; i < letterArray.length; i++){
-    letra = letterArray[i]
-    asciNumber = letra.charCodeAt(0)
-    numberArray.push(asciNumber)    
+//FUNCIÓN DECIFRAR MENSAJE
+function descifrar(){
+  xtexto = document.getElementById("inputCifrado").value
+  xnumero = document.getElementById("numeroDescifrar").value
+  const xletterArray = []
+  const xnumberArray = []
+  const xasciiArray = []
+  let xnumberToLetter = 0
+  let xmensajeDescifrado = ""
+  for (let i = 0; i < xtexto.length; i++){
+    xletterArray.push(xtexto.slice(i,1+i))
+    xnumberArray.push(xletterArray[i].charCodeAt(0))
+    xasciiArray.push(cipher.decode((+(xnumero)),xnumberArray[i]))
+    xnumberToLetter = String.fromCharCode(xasciiArray[i])
+    xmensajeDescifrado += xnumberToLetter
   }
+  return document.getElementById("textoDescifrado").innerHTML = xmensajeDescifrado
 }
 
-//función para realizar la fórmula de cifrado (sumar offset, convertir a letra)
-let numberOfNumber = ""
-let asciArray = []
-let cipheredNumber = "" 
-let cipheredMessage = ""
 
-function numberPlusOffset(){
-  for (let i =0; i < letterArray.length; i++){
-    numberOfNumber = ((((numberArray[i])-65)+(+numberCipher))%26)+65
-    asciArray.push(numberOfNumber)
-    cipheredNumber = String.fromCharCode(asciArray[i])
-    cipheredMessage += cipheredNumber 
-  }
-}
+//console.log(cipher);
+
+//document.getElementById("textoCifrado").innerHTML
+
+//boton de cifrado (boton go)
+
 
 //función para mostrar el cuadro con el texto cifrado y que lo demás no aparezca (excepto el título)
 function mostrarCipherScreen(){
@@ -88,77 +68,10 @@ function mostrarCipherScreen(){
   document.getElementById("cipherScreen").style.display = "block";
 }
 
-//función para cifrar el mensaje al hacer click en el botón cifrar
-function cifrar(){
-  getLetter()
-  letterToNumber()
-  numberPlusOffset()
-  document.getElementById("textoCifrado").innerHTML = cipheredMessage
-  mostrarCipherScreen()
-}
-
-//************************************************************************************************************************ */
-//DESCIFRAR
-//************************************************************************************************************************ */
-
-//función para guardar cada letra del input por separado en un array
-let xletterArray = [];
-let xletter = ""
-let xa = 0
-let xz = 1
-
-function getLetter2(){
-  for (let i = 0; i < inputDecipher.length; i++){
-    xletter = inputDecipher;
-    let xletterCut = xletter.slice(xa,xz);
-    xletterArray.push(xletterCut);
-    xa += 1;
-    xz += 1;
-  }
-}
-
-//función para convertir cada letra del array en un numero ascii
-let xletra = ""
-let xasciNumber = 0
-let xnumberArray = []
-
-function letterToNumber2(){
-  for (let i =0; i < xletterArray.length; i++){
-    xletra = xletterArray[i]
-    xasciNumber = xletra.charCodeAt(0)
-    xnumberArray.push(xasciNumber)    
-  }
-  return console.log(xnumberArray)
-}
-
-//función para realizar la fórmula de cifrado (sumar offset, convertir a letra)
-let xnumberOfNumber = ""
-let xasciArray = []
-let xdecipheredNumber = "" 
-let xdecipheredMessage = ""
-
-//
-
-function numberPlusOffset2(){
-  for (let i =0; i < xletterArray.length; i++){
-    xnumberOfNumber = (((((xnumberArray[i])-65)+26)-(+numberDecipher))%26)+65
-    xasciArray.push(xnumberOfNumber)
-    xdecipheredNumber = String.fromCharCode(xasciArray[i])
-    xdecipheredMessage += xdecipheredNumber 
-  }
-}
 
 //función para mostrar el cuadro con el texto cifrado y que lo demás no aparezca (excepto el título)
 function mostrarDecipherScreen(){
   document.getElementById("parteUno").style.display = "none";
   document.getElementById("parteDos").style.display = "none";
   document.getElementById("decipherScreen").style.display = "block";
-}
-
-function descifrar(){
-  getLetter2()
-  letterToNumber2()
-  numberPlusOffset2()
-  document.getElementById("textoDescifrado").innerHTML = xdecipheredMessage
-  mostrarDecipherScreen()
 }
